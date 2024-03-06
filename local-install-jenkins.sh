d3f0ec90b8e57756158324422198e2a3bd777741
Yahaya Azeez <azconcept2016@gmail.com>
2024-03-06 13:19:29 +0000

    install jenkins locally

--- local-install-jenkins.sh
+++ local-install-jenkins.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This script install jenkins and its dependencies
+# This script install jenkins and its dependencies locally
 # Author: Azeez Yahaya
 # Date: 19/2/2024
 # Usage: bash install-jenkins.sh
@@ -9,25 +9,25 @@
 # update ubuntu
 echo "updating ubuntu ..."
 sleep 2
-sudo apt update > /dev/null
+sudo apt update
 
 # install Openjdk 11 to install java 11
 echo "installing Openjdk 11 to install java 11 ..."
 sleep 2
-sudo apt install openjdk-11-jdk -y > /dev/null
+sudo apt install openjdk-11-jdk -y
 
 #  import the GPG key to verify package integrity.
 echo "importing the GPG key to verify package integrity ..."
 sleep 2
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+  /usr/share/keyrings/jenkins-keyring.asc
 
 # add the Jenkins repository and add the authentication key to the source list
 echo "adding the Jenkins repository and add the authentication key to the source list ..."
 sleep 2
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+  /etc/apt/sources.list.d/jenkins.list
 
 # updating the index
 echo "updating the index ..."
@@ -37,7 +37,7 @@ sudo apt update > /dev/null
 # initiate jenkins installation
 echo "initiate jenkins installation ..."
 sleep 2
-sudo apt-get install jenkins -y > /dev/null
+sudo apt-get install jenkins -y
 
 echo "Happy hacking!"
 sleep 2
